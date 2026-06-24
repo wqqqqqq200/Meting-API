c56c509bb7da3e573ccb4ee8211f3e602edabcc0
Q <3126197382@qq.com>
2026-06-24 10:27:10 +0800

    refactor(playlist): 优化获取歌单歌曲逻辑
    重构获取歌单歌曲的代码，使用循环处理分页请求，确保能够正确获取所有歌曲

--- src/providers/netease/playlist.js
+++ src/providers/netease/playlist.js
@@ -7,32 +7,35 @@ export const get_playlist = async (id, cookie = '') => {
         n: 100000,
         s: 8,
     }
-    let limit = 200 || Infinity
-    let offset = 0 || 0
+    const limit = 200
 
     let res = await request('POST', `https://music.163.com/api/v6/playlist/detail`, data, { crypto: 'api', cookie: cookie || {} })
 
-    let trackIds = res.playlist.trackIds
+    const trackIds = res.playlist?.trackIds || []
+    const songs = []
 
-    let idsData = {
-        c:
-            '[' +
-            trackIds
-                .slice(offset, offset + limit)
-                .map((item) => '{"id":' + item.id + '}')
-                .join(',') +
-            ']',
+    for (let offset = 0; offset < trackIds.length; offset += limit) {
+        const idsData = {
+            c:
+                '[' +
+                trackIds
+                    .slice(offset, offset + limit)
+                    .map((item) => '{"id":' + item.id + '}')
+                    .join(',') +
+                ']',
+        }
+
+        res = await request(
+            'POST',
+            `https://music.163.com/api/v3/song/detail`,
+            idsData,
+            { crypto: 'weapi', cookie: cookie || {} }
+        )
+
+        if (res.songs?.length) {
+            songs.push(...res.songs)
+        }
     }
 
-    res = await request(
-        'POST',
-        `https://music.163.com/api/v3/song/detail`,
-        idsData,
-        { crypto: 'weapi', cookie: cookie || {} }
-    )
-
-    res = map_song_list(res)
-
-    return res
-
+    return map_song_list({ songs })
 }
